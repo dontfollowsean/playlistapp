39b28b200daf9eec61e5d571c64ee680d5bf9f5a
Sean Wilkinson <sean@Seans-MacBook-Pro.local>
2018-04-21 00:56:49 -0400

    Generic controllers for api routes

--- src/api/modules/query.js
+++ src/api/modules/query.js
@@ -29,27 +29,51 @@ export const controllers = {
 }
 
 export const createOne = (model) => (req, res, next) => {
-
+  console.log(req);
+  return controllers.createOne(model, req.body)
+         .then(doc => res.status(201).json(doc))
+         .catch(err => next(err))
 }
 
 export const updateOne = (model) => async (req, res, next) => {
-
+  const docToUpdate = req.docFromId
+  const update = req.body
+  return controllers.updateOne(docToUpdate, update)
+          .then(doc => res.status(201).json(doc))
+          .catch(err => next(err))
 }
 
 export const deleteOne = (model) => (req, res, next) => {
+  const docToDelete = req.docFromId
 
+  return controllers.deleteOne(docToDelete)
+          .then(doc => res.status(201).json(doc))
+          .catch(err => next(err))
 }
 
 export const getOne = (model) => (req, res, next) => {
-
+  return controllers.getOne(req.docFromId)
+          .then(doc => res.status(200).json(doc))
+          .catch(err => next(err))
 }
 
 export const getAll = (model) => (req, res, next) => {
-
+  return controllers.getAll(model)
+          .then(docs => res.json(docs))
+          .catch(err => next(err))
 }
 
 export const findByParam = (model) => (req, res, next, id) => {
-  
+  return controllers.findByParam(model, id)
+          .then(doc => {
+            if(!doc){
+              next(new Error('Document not found'))
+            } else {
+              req.docFromId = doc
+              next()
+            }
+          })
+          .catch(err => next(err))
 }
 
 
