f81c18811f65319bd2f8e9834fb6efff038c41c3
Sean Wilkinson <sean@seans-mpb.local>
2018-05-02 02:00:08 -0400

    Add auth and error handling to api route Setup global middleware

--- src/middleware.js
+++ src/middleware.js
@@ -1,7 +1,8 @@
 import bodyParser from 'body-parser'
 
 const setGlobalMiddleware = (app) => {
-  
+  app.use(bodyParser.json())
+  app.use(bodyParser.urlencoded({extended: true}))
 }
 
 export default setGlobalMiddleware
